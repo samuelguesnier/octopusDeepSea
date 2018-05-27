7f6ecc842aeeab8657eceb83c1a918f68a705350
samuelguesnier <thenemesisa5@gmail.com>
2018-05-27 18:28:58 +0200

    media queries

--- script.js
+++
@@ -1,7 +0,0 @@
-
-(function(){
-const bgMenus = document.getElementsByClassName("hamburger")[0];
-bgMenus.addEventListener("click",()=>{
-	document.getElementById("hamburger").classList.toggle("is-active");
-})
-})();
