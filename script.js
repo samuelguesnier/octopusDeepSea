02d421353b2b13e253874a2302793091670da234
samuelguesnier <thenemesisa5@gmail.com>
2018-05-28 17:21:03 +0200

    smooth scroll

--- script.js
+++ script.js
@@ -242,3 +242,133 @@ function initMap() {
           map: map
         });
       }
+
+
+
+
+
+
+
+
+(function() {
+  let d = document;
+
+  function init() {
+    //Links 
+    let anchor1Link  = d.getElementById('oil');
+    let anchor2Link  = d.getElementById('scientific');
+    let anchor3Link  = d.getElementById('explo');
+    let anchor4Link  = d.getElementById('assit');
+    //Anchors
+    let anchor1      = d.getElementById('oilProspection');
+    let anchor2      = d.getElementById('scientificResearch');
+    let anchor3      = d.getElementById('exploration');
+    let anchor4      = d.getElementById('assitance');
+    
+    anchor1Link.addEventListener('click', (e) => { scrollTo(anchor1, e) }, false);
+    anchor2Link.addEventListener('click', (e) => { scrollTo(anchor2, e) }, false);
+   
+    anchor3Link.addEventListener('click', (e) => { scrollTo(anchor3, e) }, false);
+    
+  }
+  
+  function scrollTopValue(domElement) { //DEBUG
+    return 'scrollTopValue:', domElement.scrollTop;
+  }
+  function offsetTopValue(domElement) { //DEBUG
+    return 'offsetTopValue:', domElement.offsetTop;
+  }
+
+  var requestAnimFrame = (function() {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+  })();
+
+  function scrollTo(to, callback, duration = 1500) { 
+    
+    if (isDomElement(to)) {
+      // console.log('this is an element:', to); //DEBUG
+      to = to.offsetTop;
+    }
+  
+    function move(amount) {
+      // document.scrollingElement.scrollTop = amount; //FIXME Test that
+      document.documentElement.scrollTop = amount;
+      document.body.parentNode.scrollTop = amount;
+      document.body.scrollTop = amount;
+    }
+
+    function position() {
+      return document.documentElement.offsetTop || document.body.parentNode.offsetTop || document.body.offsetTop;
+    }
+    
+    var start = position(),
+      change = to - start,
+      currentTime = 0,
+      increment = 20;
+    console.log('start:', start); //DEBUG
+    console.log('to:', to); //DEBUG
+    console.log('change:', change); //DEBUG
+    
+    var animateScroll = function() {
+      // increment the time
+      currentTime += increment;
+      // find the value with the quadratic in-out easing function
+      var val = Math.easeInOutQuad(currentTime, start, change, duration);
+      // move the document.body
+      move(val);
+      // do the animation unless its over
+      if (currentTime < duration) {
+        requestAnimFrame(animateScroll);
+      }
+      else {
+        if (callback && typeof(callback) === 'function') {
+          // the animation is done so lets callback
+          callback();
+        }
+      }
+    };
+    
+    animateScroll();
+  }
+
+  init();
+})();
+
+
+Math.easeInOutQuad = function(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) {
+    return c / 2 * t * t + b
+  }
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+};
+
+Math.easeInCubic = function(t, b, c, d) {
+  var tc = (t /= d) * t * t;
+  return b + c * (tc);
+};
+
+Math.inOutQuintic = function(t, b, c, d) {
+  var ts = (t /= d) * t,
+    tc = ts * t;
+  return b + c * (6 * tc * ts + -15 * ts * ts + 10 * tc);
+};
+
+function isDomElement(obj) {
+    return obj instanceof Element;
+}
+
+function isMouseEvent(obj) {
+    return obj instanceof MouseEvent;
+}
+
+function findScrollingElement(element) { //FIXME Test this too
+  do {
+    if (element.clientHeight < element.scrollHeight || element.clientWidth < element.scrollWidth) {
+      return element;
+    }
+  } while (element = element.parentNode);
+}
