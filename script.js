c953978f6c43b917a77f7f4cbd24a6861dc08917
samuelguesnier <thenemesisa5@gmail.com>
2018-05-28 08:43:45 +0200

    google map

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: 53.4120954, lng: -3.0561409};
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 8,
           styles:[
   {
     "elementType": "geometry",
